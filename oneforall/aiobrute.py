0db3791f0f62c75fbf0c879ee6c692417b440ddc
shmilylty <admin@hackfun.org>
2019-08-02 19:05:35 +0800

    入口修改为使用Fire

--- oneforall/aiobrute.py
+++ oneforall/aiobrute.py
@@ -270,6 +270,6 @@ def do(domain, result):  # 统一入口名字 方便多线程调用
 
 
 if __name__ == '__main__':
-    # fire.Fire(AIOBrute)
-    result_queue = queue.Queue()
-    do('owasp.org', result_queue)
+    fire.Fire(AIOBrute)
+    # result_queue = queue.Queue()
+    # do('owasp.org', result_queue)
