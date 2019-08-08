d4e77aeac2c0efa008dccfed3b79dcb14ca86fc9
shmilylty <admin@hackfun.org>
2019-08-08 13:59:20 +0800

    设置系统变量PYTHONIOENCODING为utf-8 去掉使用reconfigure函数设置

--- oneforall/config.py
+++ oneforall/config.py
@@ -192,7 +192,9 @@ logger.level(name='ALERT', no=30, color='<yellow><bold>', icon='⚠️')
 logger.level(name='ERROR', no=40, color='<red><bold>', icon='❌️')
 logger.level(name='FATAL', no=50, color='<RED><bold>', icon='☠️')
 
-sys.stderr.reconfigure(encoding='utf-8')
+if not os.environ.get('PYTHONIOENCODING'):  # 设置编码
+    os.environ['PYTHONIOENCODING'] = 'utf-8'
+
 logger.add(sys.stderr, level='INFOR', format=stdout_fmt, enqueue=True)
 logger.add(log_path, level='TRACE', format=logfile_fmt, enqueue=True,
            encoding='utf-8')
