488e9cfce731ad8c536022980b658ae70be49b5e
Jing Ling <admin@hackfun.org>
2020-02-20 14:37:05 +0800

    typo

--- oneforall/oneforall.py
+++ oneforall/oneforall.py
@@ -51,7 +51,7 @@ class OneForAll(object):
     Example:
         python3 oneforall.py version
         python3 oneforall.py --target example.com run
-        python3 oneforall.py --target ./subdomains.txt run
+        python3 oneforall.py --target ./domains.txt run
         python3 oneforall.py --target example.com --valid None run
         python3 oneforall.py --target example.com --brute True run
         python3 oneforall.py --target example.com --port medium run
