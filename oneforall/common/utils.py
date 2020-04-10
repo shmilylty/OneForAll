4a7fab3822ebbd21ac5d4584235ce0671dab6fe8
Jing Ling <admin@hackfun.org>
2020-04-11 00:18:35 +0800

    修复传入包含批量主域的文件路径无效的问题

--- oneforall/common/utils.py
+++ oneforall/common/utils.py
@@ -124,19 +124,19 @@ def get_domains(target):
     elif isinstance(target, list):
         domains = target
     elif isinstance(target, str):
-        target = target.lower().strip()
-        domain = Domain(target).match()
-        if domain:
-            domains.append(domain)
+        path = Path(target)
+        if path.exists() and path.is_file():
+            with open(target, encoding='utf-8', errors='ignore') as file:
+                for line in file:
+                    line = line.lower().strip()
+                    domain = Domain(line).match()
+                    if domain:
+                        domains.append(domain)
         else:
-            path = Path(target)
-            if path.is_file() and path.exists():
-                with open(target, encoding='utf-8', errors='ignore') as file:
-                    for line in file:
-                        line = line.lower().strip()
-                        domain = Domain(line).match()
-                        if domain:
-                            domains.append(domain)
+            target = target.lower().strip()
+            domain = Domain(target).match()
+            if domain:
+                domains.append(domain)
     count = len(domains)
     if count == 0:
         logger.log('FATAL', f'获取到{count}个域名')
