68e7d6d76d17e64ce9da587e616ae4fbb26425e9
Jing Ling <shmilylty@gmail.com>
2020-05-13 17:17:58 +0800

    移除Brotli依赖

--- common/utils.py
+++ common/utils.py
@@ -51,7 +51,7 @@ def gen_fake_header():
     headers = {
         'Accept': 'text/html,application/xhtml+xml,'
                   'application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
         'Cache-Control': 'max-age=0',
         'Connection': 'close',
