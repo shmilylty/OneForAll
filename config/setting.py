515cf5e432dae82032a108694ca28000a22c8df2
Jing Ling <shmilylty@gmail.com>
2020-05-27 14:56:53 +0800

    参数调优

--- config/setting.py
+++ config/setting.py
@@ -146,8 +146,8 @@ fake_header = True  # 使用伪造请求头
 # 为了保证请求质量 请谨慎更改以下设置
 # request_method只能是HEAD或GET,HEAD请求方法更快，但是不能获取响应体并提取从中title及banner
 request_method = 'GET'  # 使用请求方法，默认GET
-sockread_timeout = 10  # 每个请求socket读取超时时间，默认10秒
-sockconn_timeout = 5  # 每个请求socket连接超时时间，默认5秒
+sockread_timeout = 6  # 每个请求socket读取超时时间，默认6秒
+sockconn_timeout = 3  # 每个请求socket连接超时时间，默认3秒
 # 限制同一时间打开的连接总数
 limit_open_conn = 800  # 默认800
 # 限制同一时间在同一个端点((host, port, is_ssl) 3者都一样的情况)打开的连接数
