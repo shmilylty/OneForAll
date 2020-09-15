c1ce4cbf3c994ca009e68670ad74a49ace7d0cbd
Jing Ling <shmilylty@gmail.com>
2020-09-16 00:31:16 +0800

    typos

--- modules/search/github_api.py
+++ modules/search/github_api.py
@@ -33,8 +33,7 @@ class GithubAPI(Search):
             msg = resp_json.get('message')
             logger.log('ERROR', msg)
             return False
-        else:
-            return True
+        return True
 
     def search(self):
         """
@@ -104,4 +103,4 @@ def run(domain):
 
 
 if __name__ == '__main__':
-    run('exmaple.com')
+    run('example.com')
