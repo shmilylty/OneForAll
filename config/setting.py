19724eb42ffb800b848de6b5a2b3e84eeb481462
Jing Ling <shmilylty@gmail.com>
2020-10-11 16:03:59 +0800

    typo

--- config/setting.py
+++ config/setting.py
@@ -22,8 +22,6 @@ enable_takeover_check = False  # 开启子域接管风险检查(默认False)
 http_request_port = 'small'  # HTTP请求子域(默认'small'，探测80,443端口)
 # 参数可选值True，False分别表示导出存活，全部子域结果
 result_export_alive = False  # 只导出存活的子域结果(默认False)
-# 参数可选格式有'rst', 'csv', 'tsv', 'json', 'yaml', 'html',
-# 'jira', 'xls', 'xlsx', 'dbf', 'latex', 'ods'
 result_save_format = 'csv'  # 子域结果保存文件格式(默认csv)
 # 参数path默认None使用OneForAll结果目录自动生成路径
 result_save_path = None  # 子域结果保存文件路径(默认None)
