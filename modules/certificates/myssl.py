682b26120fc40e7eafeca0ca8a368fa84fa41fab
shmilylty <shmilylty@gmail.com>
2022-11-21 17:04:11 +0800

    添加MySSL模块

---
+++ modules/certificates/myssl.py
@@ -0,0 +1,46 @@
+from common.query import Query
+
+
+class MySSL(Query):
+    def __init__(self, domain):
+        Query.__init__(self)
+        self.domain = domain
+        self.module = 'Certificate'
+        self.source = 'MySSLQuery'
+        self.addr = 'https://myssl.com/api/v1/discover_sub_domain'
+
+    def query(self):
+        """
+        向接口查询子域并做子域匹配
+        """
+        self.header = self.get_header()
+        self.proxy = self.get_proxy(self.source)
+        params = {'domain': self.domain}
+        resp = self.get(self.addr, params)
+        self.subdomains = self.collect_subdomains(resp)
+
+    def run(self):
+        """
+        类执行入口
+        """
+        self.begin()
+        self.query()
+        self.finish()
+        self.save_json()
+        self.gen_result()
+        self.save_db()
+
+
+def run(domain):
+    """
+    类统一调用入口
+
+    :param str domain: 域名
+
+    """
+    query = MySSL(domain)
+    query.run()
+
+
+if __name__ == '__main__':
+    run('freebuf.com')
