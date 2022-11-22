834005d2be5bed3731113fa63114cd949e03ad9b
shmilylty <shmilylty@gmail.com>
2022-11-22 10:36:53 +0800

    fixed #247

--- config/api.py
+++ config/api.py
@@ -78,5 +78,12 @@ github_api_token = ''
 # obtain Cloudflare API key from https://dash.cloudflare.com/profile/api-tokens
 cloudflare_api_token = ''
 
+# https://hunter.qianxin.com/home/userInfo
+hunter_api_key = ''
+
+# https://api-docs.fullhunt.io/
+fullhunt_api_key = ''
+
+
 # 登录quake之后可在个人中心获取key https://quake.360.net/quake/#/personal?tab=message
 quake_api_key = ''
