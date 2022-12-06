1622afc503c0372ad8eb09ea3012f189f5a8c356
Jing Ling <shmilylty@gmail.com>
2022-12-06 15:26:28 +0800

    解决 #286 中的导入问题

--- common/database.py
+++ common/database.py
@@ -6,7 +6,7 @@ from common import records
 
 from common.records import Connection
 from config.log import logger
-from config import settings
+from config import setting
 
 
 class Database(object):
